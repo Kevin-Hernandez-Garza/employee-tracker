b1643d3b7c213473b5a2e40caa1af1c4e471583f
Kevin Hernandez <kev.hernandezgarza@gmail.com>
2021-10-28 20:49:31 -0500

    db files created

---
+++ db/schema.sql
@@ -0,0 +1,27 @@
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS employee;
+
+CREATE TABLE department (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10,2) NOT NULL,
+    department_id INTEGER,
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER,
+    FOREIGN KEY(role_id) REFERENCES roles(id) ON DELETE SET NULL,
+    manager_id INTEGER,
+    FOREIGN KEY (manager_id) REFERENCES roles(id) ON DELETE SET NULL
+);
+
